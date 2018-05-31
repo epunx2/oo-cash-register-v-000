db41126720724d4bb625abe7952b74151cb11d6c
Eric Chrobak <eric@ericchrobak.com>
2018-05-31 02:48:57 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+## defining my cash register class which will:
+## 1. Add items of varying quantities and prices
+## 2. Calculate discounts
+## 3. Keep track of what's been added to it
+## 4. Void the last transaction
+class CashRegister
 
+  attr_accessor :discount
+  attr_reader :total
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def total
+    @total
+  end
+
+end
